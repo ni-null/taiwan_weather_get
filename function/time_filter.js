066743e2856c15598087044dbc2e2dd6a621f96e
Ninull <email@example.com>
2020-12-02 16:01:29 +0800

    修改時間格式

--- function/time_filter.js
+++ function/time_filter.js
@@ -13,7 +13,6 @@ function star_end_time_filter(startTime) {
 
   let startTime_arry = startTime.split("T");
 
-  startTime_arry[0] = startTime_arry[0].replace("2020-", "");
   startTime_arry[1] = startTime_arry[1].replace("18:00:00+08:00", "晚上");
   startTime_arry[1] = startTime_arry[1].replace("12:00:00+08:00", "午後");
   startTime_arry[1] = startTime_arry[1].replace("06:00:00+08:00", "早上");
@@ -21,8 +20,6 @@ function star_end_time_filter(startTime) {
 
 
   return (
-    startTime_arry[0] +
-    ":" +
     startTime_arry[1]
   );
 }
